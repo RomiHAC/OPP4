e04e2d874d70b70ed555a9c4125a9b53c810b0ff
The4ing <atar.orram101@gmail.com>
2025-01-09 18:28:25 +0200

    Update Board.cpp

--- src/Board.cpp
+++ src/Board.cpp
@@ -1,21 +1,16 @@
 #include "Board.h"
 
-Board::Board() : m_width(10), m_height(10) {
-    //    initializeTextures();
+Board::Board() : m_width(DEFAULT_BOARD_SIZE), m_height(DEFAULT_BOARD_SIZE) ,m_cellHeight(0), m_cellWidth(0) {
     grid.resize(static_cast<size_t>(m_width) * static_cast<size_t>(m_height));
-    // Initialize m_boardState with default values (Object::DELETE)
     m_boardState.resize(m_height, std::vector<char>(m_width, ' '));
-
 }
 
-Board::Board(int width, int height) : m_width(width), m_height(height) {
-    //   initializeTextures();
+Board::Board(int width, int height) : m_width(width), m_height(height), m_cellHeight(0), m_cellWidth(0) {
     grid.resize(static_cast<size_t>(width) * static_cast<size_t>(height));
-    // Initialize m_boardState with default values (Object::DELETE)
     m_boardState.resize(m_height, std::vector<char>(m_width, ' '));
 }
 
-size_t Board::getIndex(int x, int y) const {
+size_t Board::getIndex(const int x, const int y) const {
     return static_cast<size_t>(y) * static_cast<size_t>(m_width) + static_cast<size_t>(x);
 }
 
@@ -26,27 +21,23 @@ void Board::clearData() {
     m_height = 0;
 }
 
-void Board::initializeBoard(int cols, int rows, int W, int H) {
-    //m_toolbarHeight = TOOLBAR_HEIGHT;
+void Board::initializeBoard(const int cols, const int rows, const int W, const int H) {
+
     m_width = cols;
     m_height = rows;
-    std::cout << "m_width: " << m_width << ", m_height: " << m_height << "\n";
-    // Clear and resize grid
     grid.clear();
     grid.resize(static_cast<size_t>(m_width) * static_cast<size_t>(m_height));
-
-    // Initialize m_boardState
     m_boardState.clear();  // Clear old state
     m_boardState.resize(m_height, std::vector<char>(m_width, ' ')); // Default value
 
-    float cellW = static_cast<float>(W) / static_cast<float>(cols);
-    float cellH = (static_cast<float>(H) - TOOLBAR_HEIGHT) / static_cast<float>(rows);
+    m_cellWidth = static_cast<float>(W) / static_cast<float>(cols);
+    m_cellHeight = (static_cast<float>(H) - TOOLBAR_HEIGHT) / static_cast<float>(rows);
 
     for (int row = 0; row < m_height; ++row) {
         for (int col = 0; col < m_width; ++col) {
             size_t index = getIndex(col, row);
-            grid[index].setSize(sf::Vector2f(cellW, cellH));
-            grid[index].setPosition(col * cellW, row * cellH + TOOLBAR_HEIGHT);
+            grid[index].setSize(sf::Vector2f(m_cellWidth, m_cellHeight));
+            grid[index].setPosition(col * m_cellWidth, row * m_cellHeight + TOOLBAR_HEIGHT);
             grid[index].setFillColor(sf::Color::White);
             grid[index].setOutlineColor(sf::Color::Black);
             grid[index].setOutlineThickness(1.f);
@@ -54,29 +45,25 @@ void Board::initializeBoard(int cols, int rows, int W, int H) {
     }
 }
 
-//need to change according to int
-void Board::handleMouseClick(int mouseX, int mouseY, int selectedObject) {
-    float cellWidth = grid[0].getSize().x; // Cell width
-    float cellHeight = grid[0].getSize().y; // Cell height
+void Board::handleMouseClick(const int mouseX, const int mouseY, const int selectedObject) {
 
     // Adjust mouseY to account for the toolbar
-    int adjustedMouseY = mouseY - TOOLBAR_HEIGHT;
+    int adjustedMouseY = mouseY - static_cast<int>(TOOLBAR_HEIGHT);
 
-    int col = static_cast<int>(mouseX / cellWidth); // Calculate the column
-    int row = static_cast<int>(adjustedMouseY / cellHeight); // Calculate the row
-    // std::cout << "selectedObject: " << selectedObject << ", Object::SAVE: " << Object::SAVE << "\n";
+    int col = static_cast<int>(mouseX / m_cellWidth); 
+    int row = static_cast<int>(adjustedMouseY / m_cellHeight); 
 
     if (col >= 0 && col < m_width && row >= 0 && row < m_height) {
         size_t index = getIndex(col, row);
 
         if (selectedObject == Object::DELETE) {
-            grid[index].setFillColor(sf::Color::White); // Empty cell
-            grid[index].setTexture(nullptr); // CLEAR texture
-            m_boardState[row][col] = ' '; // Update board state to empty
+            grid[index].setFillColor(sf::Color::White); 
+            grid[index].setTexture(nullptr); 
+            m_boardState[row][col] = ' '; 
         }
 
-        else if (0 <= selectedObject <= 5) {
-            // Load the texture associated with the selected object
+        else if (selectedObject <= 5) {
+            
             const sf::Texture& texture = getTextureForObject(selectedObject);
             if (&texture) {
 
@@ -84,31 +71,28 @@ void Board::handleMouseClick(int mouseX, int mouseY, int selectedObject) {
                     updateLocationRobot();
                 }
                 else if (m_objectOrder[selectedObject] == "DOOR") {
-                    // Check if the door can be placed only on valid boundaries
+                    
                     if (row != 0 && col != 0 && row != m_boardState.size() - 1 && col != m_boardState[row].size() - 1) {
-                        std::cerr << "Invalid door placement! Doors can only be placed on the first row, first column, "
-                            "last row, or last column.\n";
-                        return;  // Exit without making changes
+                        return;
                     }
                     updateLocationDoor();
                 }
 
-                // std::cout << texture;
-                grid[index].setTexture(&texture); // Set the new texture
+                grid[index].setTexture(&texture); 
                 if (m_objectOrder[selectedObject] == "ROBOT") {
-                    m_boardState[row][col] = '/'; // Robot
+                    m_boardState[row][col] = '/'; 
                 }
                 else if (m_objectOrder[selectedObject] == "GUARD") {
-                    m_boardState[row][col] = '!'; // Guard
+                    m_boardState[row][col] = '!'; 
                 }
                 else if (m_objectOrder[selectedObject] == "WALL") {
-                    m_boardState[row][col] = '#'; // Wall
+                    m_boardState[row][col] = '#'; 
                 }
                 else if (m_objectOrder[selectedObject] == "ROCK") {
-                    m_boardState[row][col] = '@'; // Rock
+                    m_boardState[row][col] = '@'; 
                 }
                 else if (m_objectOrder[selectedObject] == "DOOR") {
-                    m_boardState[row][col] = 'D'; // Door
+                    m_boardState[row][col] = 'D'; 
                 }
                 else {
                     std::cerr << "Invalid object selected.\n";
@@ -116,9 +100,9 @@ void Board::handleMouseClick(int mouseX, int mouseY, int selectedObject) {
 
             }
             else {
-                grid[index].setTexture(nullptr); // Clear any existing texture
-                std::cerr << "Error: Texture not found for the selected object.\n";
-                m_boardState[row][col] = ' '; // Update board state to empty
+                grid[index].setTexture(nullptr); 
+                grid[index].setTexture(nullptr); 
+                m_boardState[row][col] = ' '; 
             }
         }
 
@@ -129,12 +113,12 @@ void Board::handleMouseClick(int mouseX, int mouseY, int selectedObject) {
 }
 
 void Board::updateLocationRobot() {
-    for (size_t row = 0; row < m_boardState.size(); ++row) {  // Loop through rows
-        for (size_t col = 0; col < m_boardState[row].size(); ++col) {  // Loop through columns
+    for (size_t row = 0; row < m_boardState.size(); ++row) { 
+        for (size_t col = 0; col < m_boardState[row].size(); ++col) {  
             if (m_boardState[row][col] == '/') {
                 m_boardState[row][col] = ' ';
                 size_t index = getIndex(static_cast<int>(col), static_cast<int>(row));
-                grid[index].setFillColor(sf::Color::White); // Empty cell
+                grid[index].setFillColor(sf::Color::White); 
                 grid[index].setTexture(nullptr);
             }
         }
@@ -142,43 +126,27 @@ void Board::updateLocationRobot() {
 }
 
 void Board::updateLocationDoor() {
-    for (size_t row = 0; row < m_boardState.size(); ++row) {  // Loop through rows
-        for (size_t col = 0; col < m_boardState[row].size(); ++col) {  // Loop through columns
+    for (size_t row = 0; row < m_boardState.size(); ++row) {  
+        for (size_t col = 0; col < m_boardState[row].size(); ++col) { 
             if (m_boardState[row][col] == 'D') {
-                m_boardState[row][col] = ' ';  // Clear the door ('D') cell
-                size_t index = getIndex(static_cast<int>(col), static_cast<int>(row));  // Get grid index
-                grid[index].setFillColor(sf::Color::White);  // Set to empty color
-                grid[index].setTexture(nullptr);  // Remove door texture
+                m_boardState[row][col] = ' ';  
+                size_t index = getIndex(static_cast<int>(col), static_cast<int>(row));  
+                grid[index].setFillColor(sf::Color::White);  
+                grid[index].setTexture(nullptr); 
             }
         }
     }
 }
 
-
-
-
-/// <summary>
-/// added
-/// </summary>
-/// <param name="mouseX"></param>
-/// <param name="mouseY"></param>
-/// <param name="H"></param>
-/// <param name="W"></param>
 void Board::highlightCell(const int mouseX, const int mouseY, const  int H, const int W)
 {
-    static size_t lastHighlightedIndex = std::numeric_limits<size_t>::max(); // Track last highlighted cell
-    // Consistent toolbar height
-    const float cellWidth = static_cast<float>(W) / m_width;  // Dynamically calculate cell width
-    const float cellHeight = ((static_cast<float>(H)) - TOOLBAR_HEIGHT) / m_height;  // Dynamic cell height 
+    static size_t lastHighlightedIndex = std::numeric_limits<size_t>::max(); 
+    const float cellWidth = static_cast<float>(W) / m_width;  
+    const float cellHeight = ((static_cast<float>(H)) - TOOLBAR_HEIGHT) / m_height;  
     const float adjustHeight = mouseY - TOOLBAR_HEIGHT;
 
     int col = static_cast<int>(mouseX / cellWidth);
     int row = static_cast<int>(adjustHeight / cellHeight);
-
-    //std::cout << "Mouse Position: (" << mouseX << ", " << mouseY << ")" << std::endl;
-    //std::cout << "Adjusted MouseY: " << adjustHeight << std::endl;
-    //std::cout << "Cell Dimensions: (" << cellWidth << ", " << cellHeight << ")" << std::endl;
-    //std::cout << "Target Cell: Column " << col << ", Row " << row << std::endl;
 
     // Check if the mouse is within grid bounds
     if (col >= 0 && col < m_width && row >= 0 && row < m_height) {
@@ -186,14 +154,10 @@ void Board::highlightCell(const int mouseX, const int mouseY, const  int H, cons
 
         if (lastHighlightedIndex != currentIndex) {
             if (lastHighlightedIndex != std::numeric_limits<size_t>::max()) {
-                grid[lastHighlightedIndex].setFillColor(sf::Color::White); // Reset to default
-                //  std::cout << "Reset cell at index " << lastHighlightedIndex << " to white." << std::endl;
+                grid[lastHighlightedIndex].setFillColor(sf::Color::White); 
             }
 
-            // Highlight the new cell
-            grid[currentIndex].setFillColor(sf::Color(200, 200, 200)); // Gray highlight
-            //
-            //  std::cout << "Highlighted cell at index " << currentIndex << "." << std::endl;
+            grid[currentIndex].setFillColor(sf::Color(200, 200, 200)); 
 
             lastHighlightedIndex = currentIndex;  // Update last highlighted index
         }
@@ -201,8 +165,6 @@ void Board::highlightCell(const int mouseX, const int mouseY, const  int H, cons
     else {
         if (lastHighlightedIndex != std::numeric_limits<size_t>::max()) {
             grid[lastHighlightedIndex].setFillColor(sf::Color::White); // Reset to default
-            std::cout << "Mouse out of bounds. Reset cell at index " << lastHighlightedIndex << " to white." << std::endl;
-
             lastHighlightedIndex = std::numeric_limits<size_t>::max(); // Reset tracker
         }
     }
@@ -210,7 +172,6 @@ void Board::highlightCell(const int mouseX, const int mouseY, const  int H, cons
 
 
 }
-
 
 
 void Board::draw(sf::RenderWindow& window) {
@@ -231,20 +192,13 @@ bool Board::countRowsAndCols() {
     m_height = 0;
     m_width = 0;
 
-    // Read the file line by line
     while (std::getline(file, line)) {
-        m_height++;  // Increment row count
+        m_height++; 
 
         if (m_height == 1) {
-            // The first row defines the number of columns
-            m_width = line.length();
+            m_width = static_cast<int>(line.length());
         }
-        //else if (line.length() != cols) {
-        //    // Handle inconsistent column lengths
-        //    std::cerr << "Warning: Inconsistent number of columns in row " << rows << std::endl;
-        //}
     }
-    std::cout << "Rows: " << m_height << ", Cols: " << m_width << "\n";
     file.close();
     return true;
 }
@@ -314,7 +268,6 @@ bool Board::loadFromFile(int windowWidth, int windowHeight) {
             // Safely apply the texture
             if (textureIndex >= 0 && textureIndex < static_cast<int>(m_textures.size())) {
                 grid[index].setTexture(&m_textures[textureIndex]);
-                std::cout << "Applied texture for '" << objectName << "' at row " << row << ", col " << col << std::endl;
             }
             else {
                 std::cerr << "Invalid texture index: " << textureIndex << " for object '" << objectName << "'\n";
@@ -335,7 +288,6 @@ void Board::initializeTextures(std::string name) {
     std::string filepath = name + ".png";
 
     if (currentTexture.loadFromFile(filepath)) {
-        std::cout << "Successfully loaded texture: " << filepath << std::endl;
         m_textures.push_back(currentTexture); // Store the texture
     }
     else {
@@ -394,7 +346,5 @@ bool Board::saveToFile() const {
         std::cerr << "Error occurred while writing to file: " << BOARD_FILE << "\n";
         return false;
     }
-
-    std::cout << "Board saved successfully to " << BOARD_FILE << ".\n";
     return true;
 }
