76d155a133066fafd827e97be77747483a4d58b1
The4ing <atar.orram101@gmail.com>
2025-01-09 18:27:45 +0200

    Update Board.h

--- include/Board.h
+++ include/Board.h
@@ -5,6 +5,9 @@
 #include <SFML/Graphics.hpp>
 const float TOOLBAR_HEIGHT = 150.f; // Height of the toolbarHeight
 const int NUMBER_OF_OBJECTS = 5;
+const int DEFAULT_BOARD_SIZE = 10;
+const std::string BOARD_FILE = "Board.txt";
+
 enum Object {
     DELETE = NUMBER_OF_OBJECTS,
     SAVE,
@@ -21,21 +24,13 @@ public:
     bool saveToFile() const;
     bool loadFromFile(int windowWidth, int windowHeight);
     bool CheckExistFile();
-
-    void initializeBoard(int cols, int rows, int W, int H);
-    void handleMouseClick(int mouseX, int mouseY, int selectedObject);
-
-    //added
-    /// <summary>
+    void initializeBoard(const int cols, const int rows, const int W, const int H);
+    void handleMouseClick(const int mouseX, const int mouseY, const int selectedObject);
     void highlightCell(const int mouseX, const int mouseY, const int H, const int W);
     void updateLocationRobot();
     void updateLocationDoor();
-
-
-
     void draw(sf::RenderWindow& window);
     const sf::Texture& getTextureForObject(const int& selectedObject) const;
-
     void initializeTextures(std::string name);
     int getRows() const;
     int getCols() const;
@@ -43,14 +38,16 @@ public:
 private:
     std::vector<std::vector<char>> m_boardState;
     std::vector<std::string> m_objectOrder;
+
     int m_width;
     int m_height;
+    float m_cellWidth;
+    float m_cellHeight;
     std::vector<sf::RectangleShape> grid;
-    const std::string BOARD_FILE = "Board.txt";
-    size_t getIndex(int x, int y) const;
-    bool countRowsAndCols();
     std::vector<sf::Texture> m_textures;
     std::vector<std::string> toolbarConfig;
-    std::vector<char> m_orderOfObjects;
+
+    size_t getIndex(int x, int y) const;
+    bool countRowsAndCols();
 
 };
